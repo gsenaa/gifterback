25d8df5759287cba9e6250ea2ec254162d2da90a
gsena <gsena190@gmail.com>
2021-02-22 20:09:24 -0300

    primeiro teste

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,3 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-console.log("???????????????????");
 _app2.default.listen(process.env.PORT || 3333);
