775256cdc4e26513dde3eb59106bda0a1660b6ef
gsena <gsena190@gmail.com>
2021-02-22 22:08:11 -0300

    primeiro teste

--- dist/config/dbConnection.js
+++ dist/config/dbConnection.js
@@ -1,4 +1,5 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _mongoose = require('mongoose'); var _mongoose2 = _interopRequireDefault(_mongoose);
+//import 'dotenv/config';
 
 class Database {
     constructor(){
